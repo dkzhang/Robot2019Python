40398b9b4fdfb9b67891042d11dc35e891696b57
dkzhang@Office.PC <527656264@qq.com>
2019-10-09 15:29:24 +0800

    update server

--- src/sensorDriver/ThermalImagingDataCollectionServer/server.py
+++ src/sensorDriver/ThermalImagingDataCollectionServer/server.py
@@ -1,4 +1,4 @@
-# Copyright 2018 The gRPC Authors
+# Copyright 2015 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,13 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""The reflection-enabled version of gRPC helloworld.Greeter server."""
+"""The Python implementation of the GRPC helloworld.Greeter server."""
 
 from concurrent import futures
 import logging
 
 import grpc
-from grpc_reflection.v1alpha import reflection
 
 import helloworld_pb2
 import helloworld_pb2_grpc
@@ -32,11 +31,6 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    SERVICE_NAMES = (
-        helloworld_pb2.DESCRIPTOR.services_by_name['Greeter'].full_name,
-        reflection.SERVICE_NAME,
-    )
-    reflection.enable_server_reflection(SERVICE_NAMES, server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
